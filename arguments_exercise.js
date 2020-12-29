8f0c46b9235462dd917b1748e8ac3fd503a6a9cb
ralles <ralles.liu@gmail.com>
2020-12-28 21:33:46 -0500

    figured out sum function

--- arguments_exercise.js
+++ arguments_exercise.js
@@ -6,5 +6,15 @@ function sum() {
     return s;
 }
 
-console.log(sum(1,2,3,4) === 10);
+// console.log(sum(1,2,3,4) === 10);
 
+function sumAlt(...nums) {
+    let s = 0;
+    for (let i = 0; i < nums.length; i++) {
+        s += nums[i];
+    }
+    return s;
+
+}
+
+// console.log(sumAlt(1,2,3,4, 5) === 10);
