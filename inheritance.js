4658b971e82c07bd8f8ef7be8a185791ea0afb53
ralles <ralles.liu@gmail.com>
2020-12-29 01:23:58 -0500

    finishes inheritance

--- inheritance.js
+++ inheritance.js
@@ -31,5 +31,30 @@ Function.prototype.inherits = function (parent) {
 
 
 Function.prototype.inheritsWtihCreate = function(parent) {
-    
+    this.prototype = Object.create(BaseClass.prototype);
+    this.prototype.constructor = this;
+    // from the lecture
 }
+
+function Animal (name) {
+    this.name = name;
+  };
+  
+  Animal.prototype.sayHello = function () {
+    console.log("Hello, my name is " + this.name);
+  };
+  
+  function Dog () {};
+  
+  Dog.prototype = Object.create(Animal.prototype); // Dog now inherits from Animal
+  
+  Dog.prototype.constructor = Dog // otherwise instances of Dog will have 'instance.constructor === Animal'
+  
+  Dog.prototype.bark = function () {
+    console.log("Bark!");
+  };
+  
+  const liesel = new Dog("Liesel");
+  
+  liesel.bark();
+  liesel.sayHello();
